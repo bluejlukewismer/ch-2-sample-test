ce9153d0f8624ddc78b9b0f49d476e4ec1212c78
Luke <luke.wismer@hotmail.com>
2019-04-30 17:06:34 -0400

    Initial sharing of project

---
+++ Hamburger.java
@@ -0,0 +1,51 @@
+
+/**
+ * Write a description of class Hamburger here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Hamburger
+{
+    // instance variables - replace the example below with your own
+    private int price;
+    private int numberOfPatties;
+    private boolean hasMustard;
+    private boolean hasKetchup;
+    private boolean hasCheese;
+
+    /**
+     * Constructor for objects of class Hamburger
+     */
+    public Hamburger()
+    {
+        price = 199;
+        numberOfPatties = 1;
+        hasCheese = false;
+        hasKetchup = false;
+        hasMustard = false;
+
+    }
+
+    public void addPatty()
+    {
+      {  numberOfPatties = numberOfPatties + 1;
+      } if(numberOfPatties = 1);
+    }
+        then
+
+        }
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int getNumberOfPatties()
+    {
+
+        return numberOfPatties;
+    }
+}
